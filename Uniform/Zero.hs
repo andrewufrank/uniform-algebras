9afef0ffb46aa08c7ef28f94e3ee9096b5c10d0d
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-04-01 18:28:52 +0200

    generic for zeros

--- Uniform/Zero.hs
+++ Uniform/Zero.hs
@@ -8,6 +8,8 @@
 --        , UndecidableInstances
         , OverloadedStrings
 --        , DeriveDataTypeable
+    , DefaultSignatures
+    , TypeOperators
  #-}
 {-# OPTIONS -Wall #-}
 --{-# OPTIONS -fno-warn-missing-signatures #-}
@@ -16,6 +18,8 @@
 module Uniform.Zero (module Uniform.Zero
         , module Data.Maybe
         , module Data.Either
+        , module GHC.Generics
+
         --     Zeros (..)
         -- , Text, pair, cross
         -- ,  fst3, snd3, thd3
@@ -27,10 +31,15 @@ module Uniform.Zero (module Uniform.Zero
 import Data.Text (Text)
 import Data.Maybe
 import Data.Either
---import Data.Typeable
+
+import GHC.Generics
 
 class  Zeros z where
+-- ^ a minimal algebraic type with nothing than an identity
+--  useful to identify a specific value in a type
     zero :: z
+    default zero :: (Generic z, GZero (Rep z)) => z
+    zero = to gzero
 
     isZero, notZero :: Eq z =>  z -> Bool
     isZero z = zero == z
@@ -41,11 +50,25 @@ class  Zeros z where
     -- gives a difficult to track error, because the type is not known
 
 
+class GZero a  where
+    gzero :: a x
+instance GZero U1 where
+    gzero = U1
+instance Zeros a => GZero (K1 i a) where
+    gzero = K1 zero
+instance (GZero a, GZero b) => GZero (a :*: b) where
+    gzero = gzero :*: gzero
+instance GZero a => GZero (M1 i c a) where
+    gzero = M1 gzero
+
 
 
 instance Zeros Char where zero = ' '
 --instance (Zeros a, Eq a) => Zeros [a] where zero = []
 instance Zeros () where zero = ()
+instance Zeros Bool where zero = False
+-- contrary to the Unix tradition?
+
 --instance Zeros String where zero = ""
 --instance Zeros Strings where zero = []
 instance Zeros Int where zero = 0
@@ -60,7 +83,7 @@ instance (Zeros a, Zeros b, Zeros c, Zeros d, Zeros e)
      zero = (zero, zero, zero, zero, zero)
 
 instance Zeros Text where zero = (""::Text)
-instance Zeros (Maybe a) where zero = Nothing -- added
+instance Zeros (Maybe a) where zero = Nothing
 
 --instance (Eq (g f), Zeros f, Applicative g) => Zeros (g f)
 --    where zero = pure zero
