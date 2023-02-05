dab51c776d682dac8c4781175d25747e9f541d13
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2023-02-05 11:15:25 +0100

    removed aop prelude, because it will cause problems with 9.2.5

--- Uniform/Pointless.hs
+++ Uniform/Pointless.hs
@@ -10,47 +10,48 @@
 ----------------------------------------------------------------------
 
 module Uniform.Pointless (module Uniform.Pointless
-                    , module AOPPrelude
+                    -- , module AOPPrelude
                     ) where
 
 
-import AOPPrelude (outl, outr, swap, assocl, assocr, dupl
-    , pair, cross, cond )
+-- import AOPPrelude (outl, outr, swap, assocl, assocr, dupl
+--     , pair, cross, cond )
 -- code copied from aop-prelude (partial)
     
--- const :: a -> b -> a
--- const k a = k
--- id :: a -> a
--- id a      = a
+const :: a -> b -> a
+const k a = k
+id :: a -> a
+id a      = a
 
--- outl :: (a, b) -> a
--- outl (a, _) = a
--- outr :: (a, b) -> b
--- outr (_, b) = b
--- swap :: (a, b) -> (b, a)
--- swap (a, b) = (b, a)
+outl :: (a, b) -> a
+outl (a, _) = a
+outr :: (a, b) -> b
+outr (_, b) = b
+swap :: (a, b) -> (b, a)
+swap (a, b) = (b, a)
 
--- assocl :: (a, (b, c)) -> ((a, b), c)
--- assocl (a, (b, c)) = ((a, b), c)
--- assocr :: ((a, b), c) -> (a, (b, c))
--- assocr ((a, b), c) = (a, (b, c))
+assocl :: (a, (b, c)) -> ((a, b), c)
+assocl (a, (b, c)) = ((a, b), c)
+assocr :: ((a, b), c) -> (a, (b, c))
+assocr ((a, b), c) = (a, (b, c))
 
--- dupl :: (a, (b, c)) -> ((a, b), (a, c))
--- dupl (a, (b, c)) = ((a, b), (a, c))
--- dupr :: ((a, b), c) -> ((a, c), (b, c))
--- dupr ((a, b), c) = ((a, c), (b, c))
+dupl :: (a, (b, c)) -> ((a, b), (a, c))
+dupl (a, (b, c)) = ((a, b), (a, c))
+dupr :: ((a, b), c) -> ((a, c), (b, c))
+dupr ((a, b), c) = ((a, c), (b, c))
 
--- pair :: (a -> b, a -> c) -> a -> (b, c)
--- pair (f, g) a       = (f a, g a)
--- cross :: (a -> c, b -> d) -> (a, b) -> (c, d)
--- cross (f, g) (a, b) = (f a, g b)
--- cond :: (a -> Bool) -> (a -> b, a -> b) -> a -> b
--- cond p (f, g) a     = if p a then f a else g a
+pair :: (a -> b, a -> c) -> a -> (b, c)
+pair (f, g) a       = (f a, g a)
+cross :: (a -> c, b -> d) -> (a, b) -> (c, d)
+cross (f, g) (a, b) = (f a, g b)
+cond :: (a -> Bool) -> (a -> b, a -> b) -> a -> b
+cond p (f, g) a     = if p a then f a else g a
 
--- curry :: ((a, b) -> c) -> a -> b -> c
--- curry f a b      = f (a, b)
--- uncurry :: (a -> b -> c) -> (a, b) -> c
--- uncurry f (a, b) = f a bpair :: (t -> b) -> (t, t) -> (b, b)
+curry :: ((a, b) -> c) -> a -> b -> c
+curry f a b      = f (a, b)
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f (a, b) = f a b
+-- pair :: (t -> b) -> (t, t) -> (b, b)
 
 -- my old different definitions (with prime):
 
